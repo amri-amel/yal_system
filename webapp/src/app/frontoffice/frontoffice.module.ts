b534b10b94bd49d3e03939800fdd6a99448081d0
Amel Amri <amri.amel248@gmail.com>
2022-05-30 17:53:55 +0200

    scaffold layers and pages of front office

--- webapp/src/app/frontoffice/frontoffice.module.ts
+++ webapp/src/app/frontoffice/frontoffice.module.ts
@@ -2,10 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FrontofficeRoutingModule } from './frontoffice-routing.module';
+import { GuestWrapComponent } from './layers/guest-wrap/guest-wrap.component';
+import { GuestNavbarComponent } from './layers/guest-navbar/guest-navbar.component';
+import { GuestFooterComponent } from './layers/guest-footer/guest-footer.component';
+import { CoursesPageComponent } from './layers/courses-page/courses-page.component';
+import { TrainingsessionPageComponent } from './layers/trainingsession-page/trainingsession-page.component';
+import { AboutusPageComponent } from './layers/aboutus-page/aboutus-page.component';
+import { ContactPageComponent } from './layers/contact-page/contact-page.component';
+import { CertificationPageComponent } from './layers/certification-page/certification-page.component';
+import { LandpageComponent } from './layers/landpage/landpage.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GuestWrapComponent,
+    GuestNavbarComponent,
+    GuestFooterComponent,
+    CoursesPageComponent,
+    TrainingsessionPageComponent,
+    AboutusPageComponent,
+    ContactPageComponent,
+    CertificationPageComponent,
+    LandpageComponent,
+  
+  ],
   imports: [
     CommonModule,
     FrontofficeRoutingModule
