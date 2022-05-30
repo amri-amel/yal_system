6247ac9059fffeb8137cd807b009915c2e4896d5
Amel Amri <amri.amel248@gmail.com>
2022-05-30 18:34:17 +0200

    scaffold guest navbar and footer

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuestWrapComponent } from './frontoffice/layers/guest-wrap/guest-wrap.component';
+import { LoginFormComponent } from './shared/user/login-form/login-form.component';
+import { RegisterFormComponent } from './shared/user/register-form/register-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login', component:LoginFormComponent
+  },
+  {
+    path:'signup',component:RegisterFormComponent
+  },
+  {
+    path: 'front',
+    loadChildren: () => import('./frontoffice/frontoffice.module').then(m => m.FrontofficeModule),
+    // canLoad:[AuthGuard],
+    // canActivate:[AuthGuard],
+    // canActivateChild:[AuthGuard]
+  },
+  // {
+  //   path: 'back',
+  //   loadChildren: () => import('./back-office/back-office.module').then(m => m.BackOfficeModule),
+  //   canLoad:[AuthGuard,AdminGuard],
+  //   canActivate:[AuthGuard,AdminGuard],
+  //   canActivateChild:[AuthGuard,AdminGuard]
+  // }
+  // , 
+  {
+    path: '**',
+    redirectTo: 'front',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- webapp/src/app/frontoffice/frontoffice-routing.module.ts
+++ webapp/src/app/frontoffice/frontoffice-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusPageComponent } from './layers/aboutus-page/aboutus-page.component';
+import { CertificationPageComponent } from './layers/certification-page/certification-page.component';
+import { ContactPageComponent } from './layers/contact-page/contact-page.component';
+import { CoursesPageComponent } from './layers/courses-page/courses-page.component';
+import { GuestWrapComponent } from './layers/guest-wrap/guest-wrap.component';
+import { LandpageComponent } from './layers/landpage/landpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: GuestWrapComponent,
+    children: [
+      {
+        path: 'home',
+        component: LandpageComponent
+      },
+      {
+        path: 'courses',
+        component: CoursesPageComponent,
+
+      },
+      {
+        path: 'certifications',
+        component: CertificationPageComponent,
+
+      },
+      {
+        path: 'about',
+        component: AboutusPageComponent,
+
+      },
+      {
+        path: 'contact',
+        component: ContactPageComponent,
+
+      },
+      {
+        path: '**',
+        redirectTo: 'home',
+        //  pathMatch: 'full'
+      }
+    ],
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
